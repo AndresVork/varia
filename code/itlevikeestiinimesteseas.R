fc51c70a792edb2c9ade8b1ba70c3379cd82aa3d
AndresVork <andres.vork@gmail.com>
2017-07-23 07:47:53 +0300

    uuendus

--- code/itlevikeestiinimesteseas.R
+++ code/itlevikeestiinimesteseas.R
@@ -1,4 +1,4 @@
-## Andres Võrk, 21.juuli 2017 
+## Andres Võrk, 23.juuli 2017 
 ## https://github.com/AndresVork/varia
 
 # puhastab keskkonna
@@ -9,11 +9,62 @@ library(ggplot2)
 
 #andmed on võetud ESA andmebaasist
 setwd("C:\\Users\\avork\\Documents\\GitHub\\varia\\")
-
 #Maakondade lõikes internetiühendusega leibkondade osakaalud
 df1 <- read.csv(".\\data\\IT201sm.csv", sep=";", header=FALSE)
 df1$value <-  as.numeric(as.character(df1$V3))
 
+#violin + punktid
+p <-  df1 %>% 
+  select(-V3) %>% 
+  na.omit(df) %>% 
+  #aastad kasvavas järjekorras
+  ggplot(aes(as.factor(V1), value))+
+  geom_point(aes(colour=V2)) +
+  geom_violin(alpha = 0.5) +
+  scale_y_continuous(breaks = c(0, 25, 50, 75, 100), limits=c(0,100)) +
+  #pealkirjad
+  labs(y = "Internetiühendusega leibkondade osatähtsus, %",
+       x = "",
+       title = "Internetiühendusega leibkondade jaotus üle maakondade",
+       subtitle = "Andmed: Statistikaamet, tabel IT201",
+       caption = "\nJaotus on joonistatud maakondade väärtuste keskmiste põhjal \njoonise andmed ja kood: https://github.com/AndresVork/varia")+
+  #teksti suurus ja tüüp
+  theme_minimal(base_size = 8) +
+  theme(legend.position = "none")
+p
+ggsave("joonis1.png", plot=p)
+
+#Interneti kasutavate inimeste osakaalud
+df2 <- read.csv(".\\data\\IT32sm.csv", sep=";", header=FALSE)
+q <-  df2 %>% 
+  filter(V1=="Interneti kasutamine") %>% 
+  select(-V1) %>%
+  na.omit(df) %>% 
+  #aastad kasvavas järjekorras
+  ggplot(aes(as.factor(V2), V4))+
+  geom_point(aes(colour=V3)) +
+  geom_violin(alpha = 0.5) +
+  scale_y_continuous(breaks = c(0, 25, 50, 75, 100), limits=c(0,100)) +
+  #pealkirjad
+  labs(y = "Interneti kasutajate osakaal, %",
+       x = "",
+       title = "Interneti kasutajate osakaalu jaotus üle sotsiaaldemograafiliste rühmade",
+       subtitle = "Andmed: Statistikaamet, tabel IT32",
+       caption = "\nJaotus on joonistatud erinevate (osaliselt kattuvate) sotsiaaldemograafiliste rühmade keskmiste põhjal \njoonise andmed ja kood: https://github.com/AndresVork/varia")+
+  #teksti suurus ja tüüp
+  theme_minimal(base_family =  "Roboto Condensed", base_size = 8) +
+#legend välja
+ theme(legend.position = "none")
+q
+ggsave("joonis2.png", plot=q)
+
+
+# library(plotly)
+# ggplotly(q)
+
+########################
+#teised joonise tüübid
+#boxplot
 p <-  df1 %>% 
   select(-V3) %>% 
   na.omit(df) %>% 
@@ -29,10 +80,8 @@ p <-  df1 %>%
   #teksti suurus ja tüüp
   theme_minimal(base_size = 8)
 p
-ggsave("joonis1.png", plot=p)
+# ggsave("joonis1.png", plot=p)
 
-#Interneti kasutavate inimeste osakaalud
-df2 <- read.csv(".\\data\\IT32sm.csv", sep=";", header=FALSE)
 q <-  df2 %>% 
   filter(V1=="Interneti kasutamine") %>% 
   select(-V3, -V1) %>%
@@ -51,12 +100,9 @@ q <-  df2 %>%
   #legend välja
 #  theme(legend.position = "none")
 q
-ggsave("joonis2.png", plot=q)
+# ggsave("joonis2.png", plot=q)
 
 
-
-########################
-#teised joonised
 library(forcats) #funktsiooni fct_rev() jaoks
 library(ggjoy)
 library(viridis)
@@ -121,12 +167,45 @@ df2 %>%
   geom_histogram(binwidth = 10)  +
   facet_wrap(~V2, ncol=1)
 
-#violin
-df2 %>% 
+
+##########
+#violin ilma värvideta
+p <-  df1 %>% 
+  select(-V3) %>% 
+  na.omit(df) %>% 
+  #aastad kasvavas järjekorras
+  ggplot(aes(as.factor(V1), value))+
+  geom_point() +
+  geom_violin(alpha = 0.5) +
+  scale_y_continuous(breaks = c(0, 25, 50, 75, 100), limits=c(0,100)) +
+  #pealkirjad
+  labs(y = "Internetiühendusega leibkondade osatähtsus, %",
+       x = "",
+       title = "Internetiühendusega leibkondade jaotus üle maakondade",
+       subtitle = "Andmed: Statistikaamet, tabel IT201",
+       caption = "\nJaotus on joonistatud maakondade väärtuste keskmiste põhjal \njoonise andmed ja kood: https://github.com/AndresVork/varia")+
+  #teksti suurus ja tüüp
+  theme_minimal(base_size = 8)
+p
+ggsave("joonis1.png", plot=p)
+
+#Interneti kasutavate inimeste osakaalud
+q <-  df2 %>% 
   filter(V1=="Interneti kasutamine") %>% 
   select(-V3, -V1) %>%
-  na.omit(df2) %>% 
+  na.omit(df) %>% 
   #aastad kasvavas järjekorras
   ggplot(aes(as.factor(V2), V4))+
-  #geom_violin(fill="blue")
-  geom_violin()
+  geom_point() +
+  geom_violin(alpha = 0.5) +
+  scale_y_continuous(breaks = c(0, 25, 50, 75, 100), limits=c(0,100)) +
+  #pealkirjad
+  labs(y = "Interneti kasutajate osakaal, %",
+       x = "",
+       title = "Interneti kasutajate osakaalu jaotus üle sotsiaaldemograafiliste rühmade",
+       subtitle = "Andmed: Statistikaamet, tabel IT32",
+       caption = "\nJaotus on joonistatud erinevate (osaliselt kattuvate) sotsiaaldemograafiliste rühmade keskmiste põhjal \njoonise andmed ja kood: https://github.com/AndresVork/varia")+
+  #teksti suurus ja tüüp
+  theme_minimal(base_family =  "Roboto Condensed", base_size = 8)
+q
+ggsave("joonis2.png", plot=q)
